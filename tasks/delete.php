b609bac377566a6a0ae7059a9c39f62f5f45de8d
kaljo14 <kaloyan.i.2000@icloud.com>
2022-03-19 23:45:01 +0200

    tasks/delete bug fixed

--- tasks/delete.php
+++ tasks/delete.php
@@ -14,25 +14,30 @@ $DB = $database->connect();
 
 $task = new Task($DB);
 
-$info = json_decode(file_get_contents("php://input"),true);
+$info = json_decode(file_get_contents("php://input"));
     //echo json_encode($info);
     //print_r($info);
 //  echo $info[0]["task_id"];
+try{
+for($i=0;$i<sizeof($info);$i++){
+    if($info[$i]->task_id){
+        $task->task_id =$info[$i]->task_id;
+        if($task->delete()){
+            echo json_encode(array('note'=>'Task Deleted'));
+        }
+        else{echo json_encode(array('note'=>'Task  Not Deleted'));}
 
- for($i=0;$i<sizeof($info);$i++){
-if($info[$i]["task_id"]){
-$task->task_id =$info[$i]["task_id"];
-
-
-if($task->delete()){
-    echo json_encode(array('note'=>'Task Deleted'));
+    }
+    else{echo json_encode(array('note'=>'No task ID  added not possible to delete a task'));}
 }
-else{
-    echo json_encode(array('note'=>'Task  Not Deleted'));}
-
 }
-else{
-      echo json_encode(array('note'=>'No task ID  added not possible to delete a task'));
-
-}
+catch(TypeError){
+    if($info->task_id){
+        $task->task_id =$info->task_id;
+        if($task->delete()){
+            echo json_encode(array('note'=>'Task Deleted'));
+        }
+        else{echo json_encode(array('note'=>'Task  Not Deleted'));}
+    }
+    else{echo json_encode(array('note'=>'No task ID  added not possible to delete a task'));}
 }
