fc6ad58bed003fc1dc52e1705fcb0d60c1ad05ae
kaljo14 <kaloyan.i.2000@icloud.com>
2022-03-19 22:14:01 +0200

    Made changes to read.one.php Tasks

--- tasks/read.one.php
+++ tasks/read.one.php
@@ -21,56 +21,24 @@ $task->task_id = isset($_GET['id']) ? $_GET['id'] : die();
 
   // proverka dali ima task 
 
-  if($num > 0) {
-            
-            $tasks_arr = array();
-            // $tasks_arr['data'] = array();
-            
-            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
-            extract($row);
-
-            $task_item = array(
-                
-
-                'task_name'=>$task->task_name,
-                'task_id'=>$task->task_id,
-                'tag_id'=>$id,
-                'tag_name'=>$name,
-                'tag_collor'=>$tag_color
-            );
-// print_r($task_item);
-
-            // Push to "data"
-            array_push($tasks_arr, $task_item);
-            // array_push($tasks_arr['data'], $task_item);
-            }
-
-            // Turn to JSON & output
-           $tasktitle = array('tag_name'=>$task->tag_name,'tag_id'=>$task->tag_id,);
-            //echo $task->tag_name;
-// print_r ($tasktitle);
-  $result=array_merge($tasks_arr,$tasktitle);
-  $result2=asort($result);
-
-
-
-  //print_r(asort($result));
-            //echo json_ecnode ($tasktitle);
-            //echo json_encode($result2);
-            echo json_encode($tasks_arr);
-
-
+if($num > 0) {
+  $tasks_arr = array();
+  while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+    $task_item = array(
+      'task_name'=>$task->task_name,
+      'task_id'=>$task->task_id,
+        'tag_id'=>$id,
+      'tag_name'=>$name,
+      'tag_collor'=>$tag_color
+    );
+    array_push($tasks_arr, $task_item);
   }
-  else{
-    if(isset($task->task_name)){
-        $tasks_arr=array(
-        'task_name'=>$task->task_name,
-        'task_id'=>$task->task_id,
-        'tag_name'=>null);
-        echo json_encode($tasks_arr);}
-        else{
-          echo json_encode(
-          array('message' => 'No Tasks Found '));
-        }
-  }
+  echo json_encode($tasks_arr);
+
+
+}
+else{ 
+  echo json_encode(array('message' => 'No Tasks Found'));
+        
+}
